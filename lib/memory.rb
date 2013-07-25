c1ac2bb686d1df909de4631807e3c759e1b97032
Spencer <spencerartisan@gmail.com>
2013-07-25 10:54:38 +0100

    removed duplicate specs

--- lib/memory.rb
+++ lib/memory.rb
@@ -4,8 +4,4 @@ class Memory
   include CassandraORM::Persistable
   attr_accessor :description, :state, :priority
   defaults state: 'Not started', priority: 'Medium'
-
-  def to_s
-    "#{id}. #{description}"
-  end
 end
