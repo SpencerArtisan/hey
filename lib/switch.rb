aebf2718fd295a6d8a348f982e4cdcaa9bc93db7
Spencer <spencerartisan@gmail.com>
2013-08-13 11:18:36 +0100

    Delete dead code

--- lib/switch.rb
+++ lib/switch.rb
@@ -1,35 +1,5 @@
 require 'ostruct'
 
-class Switch < BasicObject
-  def method_missing m, options
-    @switch_methods ||= {}
-    @switch_methods[m] = options[:calls] || [options[:call]]
-  end
-
-  def process instance, args
-    switch = 
-      if args.length == 0
-        :no_args
-      elsif has_switch(args)
-        get_switch(args)
-      else
-        :default
-      end
-    result = nil
-    methods = @switch_methods[switch]
-    methods.each {|method| result = instance.send method, args}
-    result
-  end
-
-  def has_switch args
-    args[0][0] == '-'
-  end
-
-  def get_switch args
-    args[0][1].to_sym
-  end
-end
-
 module SwitchSupport
   def process args = []
     code = has_switch(args) ? get_switch(args) : :default
