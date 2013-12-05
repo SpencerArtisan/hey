96c53441b7e0a20a5a49b121884365cea9b1ca6c
SpencerArtisan <spencerartisan@gmail.com>
2013-12-05 22:32:50 +0000

    add database columns for completion and appearance

--- lib/schema.rb
+++ lib/schema.rb
@@ -1,10 +1,10 @@
-require 'cassandra'
-require 'cassandra-cql'
-require 'database'
-include Database
+require 'environment'
+include CassandraORM::Database
 
 #db.execute("CREATE KEYSPACE hey_prod WITH strategy_class='org.apache.cassandra.locator.SimpleStrategy' AND strategy_options:replication_factor=1")
 
-Database.keyspace = 'hey_prod'
+#CassandraORM::Database.keyspace = 'hey'
 #db.execute "DROP COLUMNFAMILY memory"
-db.execute "CREATE COLUMNFAMILY memory (id varchar PRIMARY KEY, description varchar, priority varchar, state varchar)"
+#db.execute "CREATE COLUMNFAMILY memory (id varchar PRIMARY KEY, description varchar, priority varchar, state varchar, appear date, completed date)"
+#CassandraORM::Database.database.execute "ALTER COLUMNFAMILY memory ADD appear_on timestamp"
+CassandraORM::Database.database.execute "ALTER COLUMNFAMILY memory ADD completed_on timestamp"
